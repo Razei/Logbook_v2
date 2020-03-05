dd7a21cbbfd8cb0153215e45e88c22a9f2e95a15
Razei <jlavine@my.centennialcollege.ca>
2020-03-05 15:12:14 -0500

    Iterating over columns in excel file

--- excel_reader.py
+++ excel_reader.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-df = pd.read_excel(r'Reports2020.xlsx', sheet_name='Sheet1', usecols='D')
-print(df)
+df = pd.read_excel(r'Report Log 2020.xlsx', sheet_name='January')
+
+for index, column in df.iterrows():
+    print(column)
+
+print(' ')
