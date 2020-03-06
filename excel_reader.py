958d03a055e44f3b04903715af5bc00ba88d93b9
Razei <you@example.com>
2020-03-06 01:16:29 -0500

    Excel reader can now import data from excel documents to the database, but manual tweaking needed.

--- excel_reader.py
+++ excel_reader.py
@@ -1,8 +1,33 @@
 import pandas as pd
+import pyodbc
 
-df = pd.read_excel(r'Report Log 2020.xlsx', sheet_name='January')
+df = pd.read_excel(r'Report Log 2020.xlsx', sheet_name='February')
+df = df.fillna(value='')
 
-for index, column in df.iterrows():
-    print(column)
 
-print(' ')
+def get_connection():
+    server_string = 'LAPTOP-L714M249\\SQLEXPRESS'
+    try:
+        conn_str = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}', host=server_string,
+                                  database='ReportLog', timeout=2,
+                                  trusted_connection='Yes')  # user='Helpdesk', password='b1pa55'
+        conn = conn_str
+        return conn
+    except pyodbc.Error as err:
+        print("Couldn't connect (Connection timed out)")
+        print(err)
+        return
+
+
+if __name__ == '__main__':
+    cursor = get_connection().cursor()
+    sql_cols = ['DATE', 'NAME', 'ROOM', 'ISSUE', 'NOTE', 'RESOLUTION', 'FIXED']
+
+    cursor.executemany('INSERT INTO dbo.Reports (DATE,NAME,ROOM,ISSUE,NOTE,RESOLUTION,FIXED) VALUES (?, ?, ?, ?, ?, ?, ?)', df[sql_cols].values.tolist())
+    cursor.commit()
+
+
+
+
+
+
