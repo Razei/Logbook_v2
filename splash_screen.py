350fc1711ce012efcaba9724eb501a6405c7dde2
Razei <jlavine@my.centennialcollege.ca>
2020-03-19 14:07:41 -0400

    Splash screen style based on main logbook style

--- splash_screen.py
+++ splash_screen.py
@@ -1,17 +1,23 @@
-from PyQt5 import QtCore, uic, QtWidgets
+from PyQt5 import QtCore, uic
 
 # get type from ui file
 SplashUI, SplashBase = uic.loadUiType('splash.ui')
 
 
 class SplashScreen(SplashBase, SplashUI):
-    def __init__(self):
+    def __init__(self, theme):
         super(SplashScreen, self).__init__()
         self.setupUi(self)
 
+        # fetches the qss stylesheet path
+        theme_path = theme['theme_path']
+
+        # reads the qss stylesheet and applies it to the application
+        self.theme = str(open(theme_path, "r").read())
+        self.setStyleSheet(self.theme)
+
         flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setWindowFlags(flags)
-        self.show()
 
     def finished(self, value):
         if value:
