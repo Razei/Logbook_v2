0a40d7630f589a42633711ef656318680095b361
Razei <jlavine@my.centennialcollege.ca>
2020-04-01 05:25:50 -0400

    Refactored code to remove unnecessary database querying

--- lab_checker.py
+++ lab_checker.py
@@ -2,6 +2,7 @@ import datetime
 from ScheduleObj import ScheduleObj
 from database_handler import DatabaseHandler
 from schedule_modifier import ScheduleModifier
+import time
 
 
 # lab checker class
@@ -11,62 +12,39 @@ class LabChecker:
     def __init__(self):
         self.tFormat = "%H:%M:%S"  # 24hr by default
         self.weekday_string = self.weekday_switch(self.get_local_date().weekday())
-        self.schedules = ScheduleModifier().get_database_schedules()
+        self.schedules = ScheduleModifier.get_schedules()
+        self.open_lab_schedules = ScheduleModifier.get_open_lab_schedules()
 
     @staticmethod
     def get_local_date():
         return datetime.datetime.today()  # local system date
 
-    def set_time_format(self, t_format):
-        # local variables
-        t_format24hr = "%H:%M:%S"
-        t_format12hr = "%I:%M:%S %p"
-
-        if t_format == '12HR':
-            self.tFormat = t_format12hr
-        else:
-            self.tFormat = t_format24hr
-
     def get_today_schedule(self):
+        self.schedules = ScheduleModifier.get_schedules()
+
         # local variables
         schedule_objects = []  # for holding a list of schedule objects
+
         self.weekday_string = self.weekday_switch(self.get_local_date().weekday())  # get weekday string of today
 
-        # query stuff
-        query = f"SELECT SCHEDULE_ID, ROOM, DAY, START_TIME, END_TIME FROM dbo.Schedule WHERE DAY = '{self.weekday_string}'"
-        cursor = DatabaseHandler.execute_query(query)
-
-        # validate the cursor for empty results
-        if not DatabaseHandler.validate_cursor(cursor):
-            return
-
-        schedule_data = cursor.fetchall()
-
-        # loop through the cursor and add data to the scheduleObj class
-        for sch_time in schedule_data:
-            # the schedule object holds the room number, day, start time, and end time
-            schedule_objects.append(ScheduleObj(sch_time.ROOM.strip(), sch_time.DAY.strip(), sch_time.START_TIME.isoformat(timespec='seconds'), sch_time.END_TIME.isoformat(timespec='seconds'), sch_time.SCHEDULE_ID))
-        cursor.close()
+        if self.schedules is not None and len(self.schedules) != 0:
+            for sch in self.schedules:
+                if sch.get_day() == self.weekday_string:
+                    schedule_objects.append(sch)
         return schedule_objects
 
     def get_today_open_lab_schedule(self):
+        self.open_lab_schedules = ScheduleModifier.get_open_lab_schedules()
+
         # local variables
+        ol_schedules = self.open_lab_schedules
         schedule_objects = []  # for holding a list of schedule objects
         self.weekday_string = self.weekday_switch(self.get_local_date().weekday())  # get weekday string of today
-        # query stuff
-        query = f"SELECT SCHEDULE_ID, ROOM, DAY, START_TIME, END_TIME FROM dbo.OpenLabSchedule WHERE DAY = '{self.weekday_string}'"
-        cursor = DatabaseHandler.execute_query(query)
 
-        # validate the cursor for empty results
-        if not DatabaseHandler.validate_cursor(cursor):
-            return
-
-        schedule_data = cursor.fetchall()
-
-        # loop through the cursor and add data to the scheduleObj class
-        for sch_time in schedule_data:
-            schedule_objects.append(ScheduleObj(sch_time.ROOM.strip(), sch_time.DAY.strip(), sch_time.START_TIME.isoformat(timespec='seconds'), sch_time.END_TIME.isoformat(timespec='seconds'), sch_time.SCHEDULE_ID))
-        cursor.close()
+        if ol_schedules is not None and len(ol_schedules) != 0:
+            for sch in ol_schedules:
+                if sch.get_day() == self.weekday_string:
+                    schedule_objects.append(sch)
         return schedule_objects
 
     @staticmethod
@@ -123,80 +101,15 @@ class LabChecker:
 
         return time_left
 
-    # for handling creation and deletion of checkboxes for labs that are vacant
-    def duration_handler(self):
 
-        if self.schedules is not None and range(len(self.schedules) != 0):  # ensuring we're not looping an empty list
-            for i in range(len(self.schedules)):  # loop through all of today's schedules
-                countdown = self.lab_checker.calculate_duration(self.schedules[i])  # calculate the countdown using the current schedule object
-                room_name = self.schedules[i].get_room().strip()  # get the room name for label
-                search = room_name + 'duration' + str(i)  # room name + i (for multiple open times in the same room)
-                
-                # countdown = (datetime.timedelta(seconds=2230) + self.staticDate) - datetime.datetime.now()  # for testing (will countdown from 30 seconds)
-                if countdown is not None:
-                    label = room_name + '         ' + 'Vacant for: ' + str(countdown)
-
-                    if self.frameEmptyRooms.findChild(QtWidgets.QCheckBox, search) is None:  # check to see if the widget exists already
-                        checkBox = QtWidgets.QCheckBox(label, self)  # create a new checkbox and append the room name + countdown
-                        checkBox.setAccessibleDescription('checkBoxRoom')  # add tag for qss styling
-                        checkBox.setObjectName(search)
-                        checkBox.stateChanged.connect(self.remove_countdown)
-                        checkBox.setAccessibleName(str(self.schedules[i].get_schedule_id()))  # to link the schedule ID
-                        self.frameEmptyRooms.layout().addWidget(checkBox)  # add the checkbox to the frame
-
-                    else:  # if the widget exists already, update it
-
-                        self.frameEmptyRooms.findChild(QtWidgets.QCheckBox, search).setText(label)
-                        if countdown < datetime.timedelta(minutes=30):
-                            if countdown.seconds % 2 == 0:
-                                self.frameEmptyRooms.findChild(QtWidgets.QCheckBox, search).setAccessibleDescription('timerDanger')
-                            else:
-                                self.frameEmptyRooms.findChild(QtWidgets.QCheckBox, search).setAccessibleDescription('checkBoxRoom')
-                            self.frameEmptyRooms.findChild(QtWidgets.QCheckBox, search).setStyleSheet('')  # force a stylesheet refresh (faster than reapplying the style sheet)
-
-                    if countdown <= datetime.timedelta(seconds=1):  # countdown expired, so remove the widget
-                        self.frameEmptyRooms.findChild(QtWidgets.QCheckBox, search).setVisible(False)
-                        self.frameEmptyRooms.findChild(QtWidgets.QCheckBox, search).deleteLater()
-
-        # open labs
-        if self.open_lab_schedules is not None and range(len(self.open_lab_schedules) != 0):
-            for i in range(len(self.open_lab_schedules)):  # loop through all of today's schedules
-                countdown = self.lab_checker.calculate_duration(self.open_lab_schedules[i])  # calculate the countdown using the current schedule object
-                room_name = self.open_lab_schedules[i].get_room().strip()  # get the room name for label
-                search = room_name + str(i)  # room name + i (for multiple open times in the same room)
-
-                # countdown = (datetime.timedelta(seconds=5) + self.staticDate) - datetime.datetime.now()  # for testing
-                if countdown is not None:  # only show countdown if it's not empty
-                    label = room_name + '         ' + str(countdown)  # text for the label
-                    if self.frameOpenLabs.findChild(QtWidgets.QLabel, search) is None:  # check to see if the widget exists already
-                        label_duration = QtWidgets.QLabel(label, self)  # create a new label and append the room name + countdown
-                        label_duration.setAccessibleDescription('checkBoxRoom')  # add tag for qss styling
-                        label_duration.setObjectName(search)  # set the object name so it's searchable later
-                        label_duration.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-                        self.frameOpenLabs.layout().addWidget(label_duration)  # add the checkbox to the frame
-                    else:  # the widget exists already so just update it
-                        self.frameOpenLabs.findChild(QtWidgets.QLabel, search).setText(label)
-
-                    if countdown <= datetime.timedelta(seconds=1):  # countdown expired, so hide and remove the widget
-                        self.frameOpenLabs.findChild(QtWidgets.QLabel, search).setVisible(False)
-                        self.frameOpenLabs.findChild(QtWidgets.QLabel, search).deleteLater()
-
-
-class Countdown:
-    def __init__(self, schedule):
-        self.schedule = schedule
-        self.countdown = ''
-
-    def get_schedule(self):
-        return self.schedule
-
-
-# temporary main for testing
 if __name__ == '__main__':
-    server_string = 'DESKTOP-SIF9RD3\\SQLEXPRESS;'
-    labChk = LabChecker()
-    labChk.set_time_format('12HR')
-    schedules = labChk.get_today_schedule()
+    l = LabChecker()
+    list = l.schedules
 
-    for schedule in schedules:
-        print(labChk.room_countdown(schedule))
+    while list is not None and range(len(list) != 0):
+        for sch in list:
+            sch.countdown.calculate_duration()
+            print(sch.get_room(), sch.get_countdown().get_duration(), sch.get_countdown().get_duration_expired())
+
+        time.sleep(1)
+        print('\n\n\n')
